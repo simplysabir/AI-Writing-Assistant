c6aceb31463fab99827e80594990914741b98235
simplysabir <simplysabir@gmail.com>
2022-12-15 00:22:54 +0530

    Latest Build. Version 1.0

---
+++ README.md
@@ -0,0 +1,17 @@
+# DeepWrite AI  
+### Welcome 👋
+
+DeepWrite AI is Made with the Help of ChatGPT3 and Specific Model is Prepared for Generating Perfect Blog Post with atmost Clarity.
+
+This is Just the Version 1.0
+More Improvement Will be Implemented.
+
+Also this is OpenSource Project so Feel free to Contribute but before contributing reach out to me first.
+
+I Have also built the Extension for Using it directly in Your Notepad but it is supported only on calmly writer only yet.
+further update will  be made soon !!!!
+
+
+as Said , see ya !
+
+if you like this project, kindly give it a star.
---
+++ pages/index.js
@@ -0,0 +1,82 @@
+import { useState } from 'react';
+import Head from 'next/head';
+import Image from 'next/image';
+import buildspaceLogo from '../assets/buildspace-logo.png';
+
+const Home = () => {
+  const [userInput, setUserInput] = useState('');
+  const [apiOutput, setApiOutput] = useState('')
+  const [isGenerating, setIsGenerating] = useState(false)
+
+  const callGenerateEndpoint = async () => {
+  setIsGenerating(true);
+  
+  console.log("Calling OpenAI...")
+  const response = await fetch('/api/generate', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ userInput }),
+  });
+
+  const data = await response.json();
+  const { output } = data;
+  console.log("OpenAI replied...", output.text)
+
+  setApiOutput(`${output.text}`);
+  setIsGenerating(false);
+}
+  const onUserChangedText = (event) => {
+    console.log(event.target.value);
+    setUserInput(event.target.value);
+  };
+  return (
+    <div className="root">
+      <Head>
+        <title>DeepWrite AI</title>
+      </Head>
+      <div className="container">
+        <div className="header">
+          <div className="header-title">
+            <h1>fantastic blog generator</h1>
+          </div>
+          <div className="header-subtitle">
+            <h2>input the title of your blog below, we'll generate the rest !</h2>
+          </div>
+        </div>
+        {/* Add this code here*/}
+        <div className="prompt-container">
+          <textarea placeholder="start typing here" className="prompt-box" value={userInput} onChange={onUserChangedText} />
+            {/* New code I added here */}
+          <div className="prompt-buttons">
+          <a
+            className={isGenerating ? 'generate-button loading' : 'generate-button'}
+            onClick={callGenerateEndpoint}
+          >
+            <div className="generate">
+            {isGenerating ? <span className="loader"></span> : <p>Generate</p>}
+            </div>
+          </a>
+        </div>
+          {apiOutput && (
+            <div className="output">
+              <div className="output-header-container">
+                <div className="output-header">
+                  <h3>Output</h3>
+                </div>
+              </div>
+              <div className="output-content">
+                <p>{apiOutput}</p>
+              </div>
+            </div>
+        )}
+        
+        </div>
+      </div>
+      
+    </div>
+  );
+};
+
+export default Home;
