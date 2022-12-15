7510a14db94668e753cb352ceb8679240e1764bb
simplysabir <simplysabir@gmail.com>
2022-12-15 10:46:39 +0530

    Latest Build, Version 1.1

--- pages/_document.js
+++ pages/_document.js
@@ -4,11 +4,11 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <meta property="og:title" content="GPT-3 Writer" key="title"/>
-        <meta property="og:description" content="build with buildspace" key="description"/>
+        <meta property="og:title" content="DeepWrite AI" key="title"/>
+        <meta property="og:description" content="Sabir Khan" key="description"/>
         <meta
           property="og:image"
-          content="https://cdn.buildspace.so/courses/gpt3-writer/project-og.jpg"
+          content="https://i.pinimg.com/564x/19/bb/98/19bb9825cd713c7d58ab6d107eec9884.jpg"
         />
         <meta name="twitter:card" content="summary_large_image"></meta>
       </Head>
